bea533bce6bedf4748c762a845be4447a6ab1272
Daniel Sarafanov <sarafanovdaniel@gmail.com>
2024-02-05 18:05:03 -0500

    implement AuthContext

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,26 +1,38 @@
 'use client'
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '../context/AuthContext';
 
-type Props = {}
+function Header() {
 
-function Header({}: Props) {
+  const router = useRouter();
+  const auth = useAuth();
 
-    const router = useRouter();
+  console.log(auth?.isLoggedIn)
 
-    return (
-        <header className='sticky flex top-0 p-5 text-white justify-between items-start w-full mx-auto z-20'>
-            <div> 
-                {/* Placeholder or logo can go here */}
-            </div>
-            <div className='flex flex-row space-x-10 right-10'> 
+  return (
+    <header className='sticky flex top-0 p-5 text-white justify-between items-start w-full mx-auto z-20'>
+      <div> 
+          {/* Placeholder or logo can go here */}
+      </div>
+      <div className='flex flex-row space-x-10 right-10 font-semibold'> 
+        {auth?.isLoggedIn ? (
+          <>
+            <h1 className='cursor-pointer' onClick={() => router.push('/chat')}>Chat</h1>
+            <h1 className='cursor-pointer' onClick={async () => {
+                auth?.logout;
+                router.push('/');
+            }}>Logout</h1>
+          </>
+        ) : (
+          <>
+            <h1 className='cursor-pointer' onClick={() => router.push('/login')}>Login</h1>
+            <h1 className='cursor-pointer' onClick={() => router.push('/signUp')}>Sign Up</h1>
+          </>
+        )}
+      </div>
+    </header>
+  )
+}
 
-                <h1 className='cursor-pointer' onClick={() => router.push('/login')}>Login</h1>
-                <h1 className='cursor-pointer' onClick={() => router.push('/signUp')}>Sign Up</h1>
-
-            </div>
-        </header>
-    )
-    }
-
-export default Header
+export default Header;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import Header from "../../components/Header";
+import { AuthProvider, useAuth } from "../../context/AuthContext";
 
 export default function Home() {
+
   return (
 
-    
+    <AuthProvider>
+      <div className="bg-[#080b21] h-screen">
+        
+        {/* Header component, points users to login/sign up */}
+        <Header />
 
-    <div className="bg-[#080b21] h-screen">
-      
-      {/* Header component, points users to login/sign up */}
-      <Header />
+        {/* Going to have a home page component with probably a logo, a descritpion and a get started component*/}
 
-      {/* Going to have a home page component with probably a logo, a descritpion and a get started component*/}
+      </div>
+    </AuthProvider>
 
-    </div>
 
   );
 }
